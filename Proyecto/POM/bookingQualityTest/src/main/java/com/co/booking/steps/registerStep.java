7552e57bc80f324f026332cfd77b3701c9eade10
Juan Esteban Cajiao Madero <96344956+BOTHIKIGAI@users.noreply.github.com>
2023-09-27 09:00:11 -0500

    creacion registre

--- Proyecto/POM/bookingQualityTest/src/main/java/com/co/booking/steps/registerStep.java
+++ Proyecto/POM/bookingQualityTest/src/main/java/com/co/booking/steps/registerStep.java
@@ -1,6 +1,61 @@
 package com.co.booking.steps;
 import com.co.booking.pageobject.registerPage;
+import net.thucydides.core.annotations.Step;
+
 public class registerStep {
     registerPage registerPage = new registerPage();
 
+    // OPEN BROWSER
+    @Step
+    public void openBrowser() {
+        registerPage.open();
+    }
+
+    // CLOSE POP-UP MESSAGE
+    @Step
+    public void CLICK_POP_UP() {
+        registerPage.getDriver().findElement(registerPage.getCLOSEPOPPUPBUTTON()).click();
+    }
+
+    // CLICK ON SING UP
+    @Step
+    public void CLICK_BUTTONSINGUP() {
+        registerPage.getDriver().findElement(registerPage.getBUTTONSINGUP()).click();
+    }
+
+    // INSERT EMAIL
+    @Step
+    public void SENDKEYS_INPUTMAIL() {
+        registerPage.getDriver().findElement(registerPage.getINPUTMAIL()).sendKeys("jagarcia8578@soy.sena.edu.co");
+    }
+
+    // CLICK ON BUTTON CONTINUE MAIL
+    @Step
+    public void CLICK_BUTTONCONTINUEMAIL() {
+        registerPage.getDriver().findElement(registerPage.getBUTTONCONTINUEMAIL()).click();
+    }
+
+    // INSERT PASSWORD
+    @Step
+    public void SENDKEYS_INPUTPASS() {
+        registerPage.getDriver().findElement(registerPage.getINPUTPASS()).sendKeys("Prueba123456");
+    }
+
+    // PASSWORD CHECK
+    @Step
+    public void SEND_INPUTCONFIRMPASS() {
+        registerPage.getDriver().findElement(registerPage.getINPUTCONFIRMPASS()).sendKeys("Prueba123456");
+    }
+
+    // CLICK BUTTON CREATE ACCOUNT
+    @Step
+    public void CLICK_BUTTONCREATEACCOUNT() {
+        registerPage.getDriver().findElement(registerPage.getBUTTONCREATEACCOUNT()).click();
+    }
+
+    // HEADER "YOUT ACCOUNT"
+    @Step
+    public void VALIDATION_HEADERYOURACCOUNT() {
+            registerPage.getDriver().findElement(registerPage.getHEADERACCOUNT());
+    }
 }
--- Proyecto/POM/bookingQualityTest/src/test/java/com/co/booking/stepdefinitions/registerStepDefinition.java
+++ Proyecto/POM/bookingQualityTest/src/test/java/com/co/booking/stepdefinitions/registerStepDefinition.java
@@ -12,16 +12,22 @@ public class registerStepDefinition {
 
     @Given("He is on the registration page")
     public void heIsOnTheRegistrationPage() {
-
+        registerStep.openBrowser();
     }
 
     @When("He enters the data from the form")
     public void heEntersTheDataFromTheForm() {
-
+        registerStep.CLICK_POP_UP();
+        registerStep.CLICK_BUTTONSINGUP();
+        registerStep.SENDKEYS_INPUTMAIL();
+        registerStep.CLICK_BUTTONCONTINUEMAIL();
+        registerStep.SENDKEYS_INPUTPASS();
+        registerStep.SEND_INPUTCONFIRMPASS();
+        registerStep.CLICK_BUTTONCREATEACCOUNT();
     }
 
     @Then("He will enter the user page")
     public void heWillEnterTheUserPage() {
-
+        registerStep.VALIDATION_HEADERYOURACCOUNT();
     }
 }
