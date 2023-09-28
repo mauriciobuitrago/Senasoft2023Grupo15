5820ede1f7413c953bda03e5838486f46f61923b
Juan Esteban Cajiao Madero <96344956+BOTHIKIGAI@users.noreply.github.com>
2023-09-28 11:29:00 -0500

    comiit

--- Proyecto/SCREENPLAY/src/main/java/com/co/booking/task/Reservation.java
+++ Proyecto/SCREENPLAY/src/main/java/com/co/booking/task/Reservation.java
@@ -1,6 +1,7 @@
 package com.co.booking.task;
 
 import com.co.booking.userinterfaces.MenuComponent;
+import com.co.booking.userinterfaces.ReservationRecordPage;
 import com.co.booking.userinterfaces.SearchPage;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
@@ -8,6 +9,7 @@ import net.serenitybdd.screenplay.Tasks;
 import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.actions.DoubleClick;
 import net.serenitybdd.screenplay.actions.Scroll;
+import net.serenitybdd.screenplay.questions.WebElementQuestion;
 
 public class Reservation implements Task {
 
@@ -32,18 +34,34 @@ public class Reservation implements Task {
                 Click.on(SearchPage.BTN_VALOR_PERSON),
                 DoubleClick.on(SearchPage.BTN_VALOR_CHILDREN),
                 Click.on(SearchPage.BTN_YEAR_FIRST_CHILDREN),
-                Click.on(SearchPage.OP_YEAR_FIRST_CHILDREN),
+                WebElementQuestion,the()
                 Click.on(SearchPage.BTN_YEAR_SECOND_CHILDREN),
                 Click.on(SearchPage.OP_YEAR_SECOND_CHILDREN),
                 Click.on(SearchPage.BTN_VALOR_BEDROOMS),
                 Click.on(SearchPage.BTN_SCROLL));
+
+// Click.on(SearchPage.OP_YEAR_FIRST_CHILDREN),
+
+
         try {
             Thread.sleep(3000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
 
+        // Select Hotel
 
+        actor.attemptsTo(Scroll.to(ReservationRecordPage.BTN_THREESTARS));
+        actor.attemptsTo(Click.on(ReservationRecordPage.BTN_THREESTARS));
+        actor.attemptsTo(Click.on(ReservationRecordPage.BTN_FOURSTARS));
+        actor.attemptsTo(Click.on(ReservationRecordPage.BTN_FIVESTARS));
+        actor.attemptsTo(Scroll.to(ReservationRecordPage.BTN_SELECTFILTER));
+        actor.attemptsTo(Click.on(ReservationRecordPage.BTN_SELECTFILTER));
+        actor.attemptsTo(Click.on(ReservationRecordPage.BTN_SELECTFILTERECONOMY));
+        actor.attemptsTo(Click.on(ReservationRecordPage.BTN_VERDISPONIBILIDAD));
+        actor.attemptsTo(Click.on(ReservationRecordPage.BTN_SELECTRESERVATION));
+        actor.attemptsTo(Click.on(ReservationRecordPage.BTN_SELECTRESERVATIONPRICE));
+        actor.attemptsTo(Click.on(ReservationRecordPage.BTN_CONFIRMRESERVATION));
 
     }
 
--- Proyecto/SCREENPLAY/src/main/java/com/co/booking/userinterfaces/ReservationRecordPage.java
+++ Proyecto/SCREENPLAY/src/main/java/com/co/booking/userinterfaces/ReservationRecordPage.java
@@ -4,10 +4,46 @@ import net.serenitybdd.screenplay.targets.Target;
 
 public class ReservationRecordPage {
 
-    // three stars
+    // THREE STARS
     public static final Target BTN_THREESTARS = Target.the("3 estrellas...")
             .locatedBy("//div [@data-filters-item = 'class:class=3']//input [@name = 'class=3']");
 
-    //
+    // FOUR STARS
+    public static final Target BTN_FOURSTARS = Target.the("4 estrellas...")
+            .locatedBy("//div [@data-filters-item = 'class:class=4']//input [@name = 'class=4']");
+
+    // FOUR STARS
+    public static final Target BTN_FIVESTARS = Target.the("5 estrellas...")
+            .locatedBy("//div [@data-filters-item = 'class:class=5']//input [@name = 'class=5']");
+
+
+    // SELECT FILTER
+    public static final Target BTN_SELECTFILTER = Target.the("Ordenar por:")
+            .locatedBy("//button [@data-testid='sorters-dropdown-trigger]");
+
+    // OPTION ECONOMY
+
+    public static final Target BTN_SELECTFILTERECONOMY = Target.the("Precio: mas bajo primero")
+            .locatedBy("//button [@data-id='price']");
+
+
+    // See availability
+    public static final Target BTN_VERDISPONIBILIDAD = Target.the("Ver disponibilidad")
+            .locatedBy("//a [@data-testid = 'availability-cta-btn']");
+
+    // Select reservation
+    public static final Target BTN_SELECTRESERVATION = Target.the("0")
+            .locatedBy("//select [@data-testid='select-room-trigger']");
+
+
+    // Select Price
+    public static final Target BTN_SELECTRESERVATIONPRICE = Target.the("1 (COP 89.260)")
+            .locatedBy("//select [@data-testid='select-room-trigger']//option [@value = '1']");
+
+    // Confirm reservation
+    public static final Target BTN_CONFIRMRESERVATION = Target.the("1 (COP 89.260)")
+            .locatedBy("//button [@id='b_tt_holder_1']");
+
+
 
 }
